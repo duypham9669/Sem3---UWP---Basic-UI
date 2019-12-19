adedc24b17e6ce70f2ced600e2fb3059772263f3
duypham9669 <44285940+duypham9669@users.noreply.github.com>
2019-12-19 21:49:28 +0700

    again

---
+++ ASM_SEM3_UWP/service/serviceIpml/loginServiceImpl.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASM_SEM3_UWP.service.serviceIpml
+{
+    class loginServiceImpl : loginService
+    {
+        public void doCheckLogin()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ASM_SEM3_UWP/service/serviceIpml/registerServiceImpl.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASM_SEM3_UWP.service.serviceIpml
+{
+    class registerServiceImpl : registerService
+    {
+        public void register()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
